0298231d06eaa49ad8d94581c86ab046f5b80a63
Telxey <131807761+Telxey@users.noreply.github.com>
2023-10-15 02:01:38 -0400

    Update setup.sh

--- stacks/wghole/setup.sh
+++ stacks/wghole/setup.sh
@@ -8,20 +8,16 @@ mkdir /home/docker && mkdir /home/docker/secrets
 cd .. 
 cd docker
 wget -O wghole.yaml https://raw.githubusercontent.com/Telxey/Docker/main/stacks/wghole/wghole.yaml
-wget -O secrets/db.env.sample  https://raw.githubusercontent.com/Telxey/Docker/main/stacks/wghole/secrets/db.env.sample
-wget -O secrets/server.env.sample https://raw.githubusercontent.com/Telxey/Docker/main/stacks/wghole/secrets/server.env.sample
+wget -O secrets/db_root_pwd.txt  https://raw.githubusercontent.com/Telxey/Docker/main/stacks/wghole/secrets/db_root_pwd.txt
+wget -O secrets/mysql_pwd.txt https://raw.githubusercontent.com/Telxey/Docker/main/stacks/wghole/secrets/mysql_pwd.txt
+wget -O secrets/pihole_pwd.txt  https://raw.githubusercontent.com/Telxey/Docker/main/stacks/wghole/secrets/pihole_pwd.txt
+wget -O secrets/wg_easy_pwd.txt https://raw.githubusercontent.com/Telxey/Docker/main/stacks/wghole/secrets/wg_easy_pwd.txt
 sleep 5
-nano secrets/db.env.sample
-nano secrets/server.env.sample
-if [ ! -f secrets/db.env ]
-then
-  cp secrets/db.env.sample secrets/db.env
-fi
-sleep 5
-if [ ! -f secrets/server.env ]
-then
-  cp secrets/server.env.sample secrets/server.env
-fi
+nano secrets/db_root_pwd.txt
+nano secrets/mysql_pwd.txt
+nano secrets/pihole_pwd.txt
+nano secrets/wg_easy_pwd.txt
+
 cp wghole.yaml docker-compose.yml
 
 docker compose up -d
@@ -32,12 +28,18 @@ cat <<EOF
          http://HOST_IP:81
          defaul-user = admin@example.com
          dfault password = changeme
-         
+EOF
+cat <<EOF
      Completed Successfully install pi-hole
      should be reachable by going to the following URL.
          http://HOST_IP:5353/admin
-         
+        PI-HOLE Web UI Password below
+EOF
+cat secrets/pihole_pwd.txt
+cat <<EOF
      Completed Successfully install wireguard-UI
      should be reachable by going to the following URL.
-         http://HOST_IP:51821        
+         http://HOST_IP:51821 
+       Wireguard-UI Password below  
 EOF
+cat secrets/wg_easy_pwd.txt
