65ac67ec878c1613f82b27c7de5f861efa692f0e
Telxey <131807761+Telxey@users.noreply.github.com>
2023-10-14 22:54:57 -0400

    Update setup.sh

--- stacks/wghole/setup.sh
+++ stacks/wghole/setup.sh
@@ -15,9 +15,30 @@ _|"""""|_|"""""|_|     |_|"""""|_|"""""|_|"""""|_|"""""|
  
 EOF
 }
-mkdir /home/docker
+mkdir /home/docker && mkdir /home/docker/secrets
 cd /home/docker
-wget -O https://github.com/Telxey/Docker/blob/main/stacks/wghole/wghole.yaml
-nano  wghole.yaml
+wget -O wghole.yaml https://github.com/Telxey/Docker/blob/main/stacks/wghole/wghole.yaml
+wget -O secrets/db.env.sample  https://raw.githubusercontent.com/Telxey/Docker/main/stacks/wghole/secrets/db.env.sample
+wget -O secrets/server.env.sample https://raw.githubusercontent.com/Telxey/Docker/main/stacks/wghole/secrets/server.env.sample
+nano secrets/db.env.sample
+nano secrets/server.env.sample
+if [ ! -f ./secrets/db.env ]
+then
+  cp ./secrets/db.env.sample ./secrets/db.env
+fi
+if [ ! -f ./secrets/server.env ]
+then
+  cp ./secrets/server.env.sample ./secrets/server.env
+fi
 docker compose -f wghole.yaml up -d
+
+msg_ok "Completed Successfully install nginix proxy manager!\n"
+echo -e "${APP} should be reachable by going to the following URL.
+         ${BL}http://${IP}:81${CL} \n"
+msg_ok "Completed Successfully install pi-hole!\n"
+echo -e "${APP} should be reachable by going to the following URL.
+         ${BL}http://${IP}:5353/admin${CL} \n"  
+msg_ok "Completed Successfully install wireguard-UI!\n"
+echo -e "${APP} should be reachable by going to the following URL.
+         ${BL}http://${IP}:51821${CL} \n"         
 
